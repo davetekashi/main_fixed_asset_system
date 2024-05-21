0fff006792c52e88e4ae7c80b74f3aad8a255bf9
Badboy Dave <davexxarson@gmail.com>
2024-05-21 21:14:04 +0100

    first commit

---
+++ inventory/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from .forms import ItemForm
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'inventory/item_list.html', {'items': items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'inventory/item_detail.html', {'item': item})
+
+def item_new(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = ItemForm()
+    return render(request, 'inventory/item_edit.html', {'form': form})
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'inventory/item_edit.html', {'form': form})
+
